b9d2e6601f9df928ca46c00671d65155ed8a63b4
Kaynec <68915270+Kaynec@users.noreply.github.com>
2021-02-17 03:44:35 +0330

    Add files via upload

--- index.js
+++ index.js
@@ -9,25 +9,10 @@ const content = document.getElementById('content')
 
 content.addEventListener('click',(e)=>{
     if (e.target.id==='main'){
-        content.innerHTML=''
-        content.classList.add('transition')
-        setTimeout(()=>{
-        content.classList.remove('transition')
-        },4000);
         displayController()
     }else if (e.target.id==='Menu'){
-        content.innerHTML=''
-        content.classList.add('transition')
-        setTimeout(()=>{
-        content.classList.remove('transition')
-        },4000);
         workFunction()
     }else if (e.target.id==='contact'){
-        content.innerHTML=''
-        content.classList.add('transition')
-        setTimeout(()=>{
-        content.classList.remove('transition')
-        },4000);
         contact()
     }
 })
